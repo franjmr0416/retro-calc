50d7176297b0940c8bd6d42d1806de208abe102c
Francisco <francisco@pop-os.localdomain>
2022-10-02 02:35:19 -0500

    styled button

---
+++ src/components/Button.js
@@ -0,0 +1,46 @@
+import styled from "styled-components";
+
+const SecondButton = styled.div`
+  width: 65px;
+  height: 65px;
+  border-radius: 5.2px;
+  background-color: #f0f0f0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  box-shadow: 0 5.2px 10.4px 0 rgba(0, 0, 0, 0.35),
+    0 2.6px 3.9px 0 rgba(0, 0, 0, 0.25), inset 0 -2px 0 0 rgba(62, 64, 65, 0.5);
+  background-blend-mode: overlay, normal;
+  background-image: linear-gradient(
+      to left,
+      #fff 100%,
+      rgba(255, 255, 255, 0) 0%
+    ),
+    linear-gradient(to bottom, #f0f0f0, #f0f0f0);
+`;
+
+const Styledbutton = styled.button`
+  font-family: Jura;
+  font-size: 31.2px;
+  font-weight: 600;
+  text-align: center;
+  color: #1e1e1e;
+  width: 45.89px;
+  height: 45.89px;
+  border: none;
+  border-radius: 10.45px;
+  cursor: pointer;
+  box-shadow: 0 2.6px 3.9px 0 rgba(0, 0, 0, 0.25),
+    inset 0 -2px 0 0 rgba(62, 64, 65, 0.5);
+  background-image: linear-gradient(to top, #fff 50%, rgba(255, 255, 255, 0)),
+    linear-gradient(to bottom, #f0f0f0, #f0f0f0);
+`;
+
+const Button = ({ children, ...props }) => {
+  return (
+    <SecondButton>
+      <Styledbutton {...props}>{children}</Styledbutton>
+    </SecondButton>
+  );
+};
+export default Button;
